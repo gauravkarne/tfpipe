38608f3b9d6b7193f033c70472ca3db77a948461
gauravkarne <148445337+gauravkarne@users.noreply.github.com>
2023-12-13 07:06:26 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,9 @@
 resource "aws_s3_bucket" "bucket1" {
-	bucket = "neeha-first-bucket"
+	bucket = "gk11-first-bucket"
 }
 
 resource "aws_s3_bucket" "bucket2" {
+	bucket = "gk22-first-bucket"
     
 }
 
